23e0688171b462634fd744a771ed47a8f3c6d7a1
hnts03 <chlrjsdn0320@gmail.com>
2021-03-20 00:16:15 +0900

    Update omok.py
    잠깐

--- omok.py
+++ omok.py
@@ -1,1 +1,18 @@
-print("오목에서 필요한건?")
+import pygame as pg
+import numpy as np
+
+pygame.init()
+
+# Define colors for using
+
+
+# Set the height and width of screen
+size = [400,400]
+screen = pg.display.set_mode(size)
+
+pg.display.set_caption("Game-Title")
+
+
+#Loop until user click the close button
+done = False
+clock = pg.time.Clock()
