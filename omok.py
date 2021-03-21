8b89cbd0e39b086cb2daa3956e92a60fe24631d9
hnts03 <chlrjsdn0320@gmail.com>
2021-03-21 22:20:28 +0900

    slightly plus

--- omok.py
+++ omok.py
@@ -16,3 +16,13 @@ pg.display.set_caption("Game-Title")
 #Loop until user click the close button
 done = False
 clock = pg.time.Clock()
+
+while not done:
+    # This limits the while loop to the max of 10 times per second
+    # Leave this out and we will use all CPU we can
+    clock.tick(10)
+
+    #Main Event Loop
+    for event in pygame.event.get(): # User do something
+        if event.type == pygame.QUIT: # If use clicked close
+            done=True # Flag that we are done so we exit this loop
