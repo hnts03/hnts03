81915e1d6e7093458c9844ad3a5b02f8e57388d3
hnts03 <chlrjsdn0320@gmail.com>
2021-03-25 00:15:40 +0900

    Update Omokfunc.py
    win check part 1

--- Omokfunc.py
+++ Omokfunc.py
@@ -1,6 +1,31 @@
 import numpy as np
 
-def checkwin(a):
+def checkwin(int[] a):
+    for i in range(a):
+        for j in range(a):
+            if a(i,j) == 1:
+                int count = 0
+                # 4type of checking process
+                # vertical check
+                for k in range(i-4,i+5):
+                    if a(k,j) == 1:
+                        count += 1
+                    else:
+                        count = 0
+
+                # horizontal check
+                for k in range(i-4,i+4):
+                    if a(i,k) == 1:
+                        count += 1
+                    else:
+                        count = 0
+
+                # left-up diagonal check
+                for k in range ()
+
+
+                    
+
     return a
 
 
